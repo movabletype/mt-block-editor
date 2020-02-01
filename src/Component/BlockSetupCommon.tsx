7a26d4d3f570398d00c79ce1b323bc9a40cae090
Taku AMANO <taku@taaas.jp>
2020-02-02 07:17:50 +0900

    Tweaks style.

--- src/Component/BlockSetupCommon.tsx
+++ src/Component/BlockSetupCommon.tsx
@@ -23,7 +23,7 @@ const BlockSetupCommonInternal: React.FC<EditorProps> = ({
     <>
       {keys.map(k => (
         <BlockSetup block={block} key={k}>
-          <label style={{ width: "100%" }}>
+          <label style={{ display: "inline-block", width: "100%" }}>
             <div>{t(labelMap[k])}</div>
             <input type="text" name={k} style={{ width: "100%" }} />
           </label>
