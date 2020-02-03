d9f0b53d54ff525d7a600d0b8055c4896dc08bf2
Taku AMANO <taku@taaas.jp>
2020-02-03 11:41:24 +0900

    Tweaks label definition.

--- src/Component/BlockLabel.tsx
+++ src/Component/BlockLabel.tsx
@@ -15,17 +15,19 @@ const BlockLabel: React.FC<EditorProps> = ({
   const { editor } = useEditorContext();
 
   return (
-    <label>
-      {editor.opts.mode === "composition" ? (
-        <>
-          <div className="mt-block-editor-label-block">{block.label}</div>
-          <div className="mt-block-editor-help-block">{block.helpText}</div>
-        </>
-      ) : (
-        <div>{t("Default Value")}</div>
-      )}
+    <>
+      <label style={{ display: "block" }}>
+        {editor.opts.mode === "composition" ? (
+          <>
+            <div className="mt-block-editor-label-block">{block.label}</div>
+            <div className="mt-block-editor-help-block">{block.helpText}</div>
+          </>
+        ) : (
+          <div>{t("Default Value")}</div>
+        )}
+      </label>
       {children}
-    </label>
+    </>
   );
 };
 
