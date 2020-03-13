6e2aabcdf5e147dcb17e17337160a58e93b4a110
Taku Amano <taku@taaas.jp>
2020-03-13 17:09:02 +0900

    Apply reset css in preview block.

--- src/Component/BlockItem.tsx
+++ src/Component/BlockItem.tsx
@@ -183,6 +183,50 @@ const BlockItem: React.FC<Props> = ({
             <div className="content-label">{b.contentLabel()}</div>
             <root.div>
               <div className="entry">
+              <style type="text/css">{`
+html, body, div, span, applet, object, iframe,
+h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+a, abbr, acronym, address, big, cite, code,
+del, dfn, em, img, ins, kbd, q, s, samp,
+small, strike, strong, sub, sup, tt, var,
+b, u, i, center,
+dl, dt, dd, ol, ul, li,
+fieldset, form, label, legend,
+table, caption, tbody, tfoot, thead, tr, th, td,
+article, aside, canvas, details, embed, 
+figure, figcaption, footer, header, hgroup, 
+menu, nav, output, ruby, section, summary,
+time, mark, audio, video {
+	margin-top: 0;
+	margin-bottom: 0;
+	padding-top: 0;
+	padding-bottom: 0;
+	border: 0;
+	font-size: 100%;
+	font: inherit;
+	vertical-align: baseline;
+}
+/* HTML5 display-role reset for older browsers */
+article, aside, details, figcaption, figure, 
+footer, header, hgroup, menu, nav, section {
+	display: block;
+}
+body {
+	line-height: 1;
+}
+blockquote, q {
+	quotes: none;
+}
+blockquote:before, blockquote:after,
+q:before, q:after {
+	content: '';
+	content: none;
+}
+table {
+	border-collapse: collapse;
+	border-spacing: 0;
+}
+`}</style>
                 {editor.opts.stylesheets.map(s => (
                   <link rel="stylesheet" key={s} href={s} />
                 ))}
