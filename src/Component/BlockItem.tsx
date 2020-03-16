948b30e696df8eef1f71299af69215c8eefaa230
Taku Amano <taku@taaas.jp>
2020-03-16 11:17:16 +0900

    Tweaks reset css.

--- src/Component/BlockItem.tsx
+++ src/Component/BlockItem.tsx
@@ -38,10 +38,6 @@ time, mark, audio, video {
 	margin-bottom: 0;
 	padding-top: 0;
 	padding-bottom: 0;
-	border: 0;
-	font-size: 100%;
-	font: inherit;
-	vertical-align: baseline;
 }
 /* HTML5 display-role reset for older browsers */
 article, aside, details, figcaption, figure, 
