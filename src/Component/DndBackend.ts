91f5d8d8e163ed8dfb90437851eddda6578c0e79
Taku Amano <taku@taaas.jp>
2020-04-15 09:42:43 +0900

    Fix typo.

--- src/Component/DndBackend.ts
+++ src/Component/DndBackend.ts
@@ -2,5 +2,5 @@ import { isTouchDevice } from "../util";
 import HTML5Backend from "react-dnd-html5-backend";
 import TouchBackend from "react-dnd-touch-backend";
 
-export const DndBackend = isTouchDevice ? TouchBackend : HTML5Backend;
+export const DndBackend = isTouchDevice() ? TouchBackend : HTML5Backend;
 export const featurePreview = DndBackend === HTML5Backend;
