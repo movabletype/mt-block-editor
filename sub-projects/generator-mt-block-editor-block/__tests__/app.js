cd0345263b4ae31d870e08bfca05ca34fe1a6430
Taku Amano <taku@taaas.jp>
2020-04-28 17:39:49 +0900

    Update sub-projects/generator-mt-block-editor-block/__tests__/app.js.

--- sub-projects/generator-mt-block-editor-block/__tests__/app.js
+++ sub-projects/generator-mt-block-editor-block/__tests__/app.js
@@ -1,16 +1,16 @@
-'use strict';
-const path = require('path');
-const assert = require('yeoman-assert');
-const helpers = require('yeoman-test');
+"use strict";
+const path = require("path");
+const assert = require("yeoman-assert");
+const helpers = require("yeoman-test");
 
-describe('generator-mt-block-editor-block:app', () => {
+describe("generator-mt-block-editor-block:app", () => {
   beforeAll(() => {
     return helpers
-      .run(path.join(__dirname, '../generators/app'))
+      .run(path.join(__dirname, "../generators/app"))
       .withPrompts({ someAnswer: true });
   });
 
-  it('creates files', () => {
-    assert.file(['dummyfile.txt']);
+  it("creates files", () => {
+    assert.file(["package.json"]);
   });
 });
