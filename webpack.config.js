8ef5f3c72241a1fe761eb2f6f679b41c0c6afb6b
Taku Amano <taku@taaas.jp>
2020-05-04 07:00:07 +0900

    Install optimize-css-assets-webpack-plugin.

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,9 @@ const path = require("path");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
-module.exports = async function(_, env) {
+module.exports = async function (_, env) {
   const isProd = env.mode === "production";
 
   return {
@@ -70,6 +71,7 @@ module.exports = async function(_, env) {
             },
           },
         }),
+        new OptimizeCssAssetsPlugin(),
       ],
     },
     devServer: {
