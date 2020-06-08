75c7c2719384b2f4f986d447b8f0cf54ebc3cd7f
Taku Amano <taku@taaas.jp>
2020-06-08 12:20:22 +0900

    Update demo.

--- docs/demo/register-block.js
+++ docs/demo/register-block.js
@@ -20,11 +20,83 @@ document.addEventListener("DOMContentLoaded", async () => {
   })
 );
 
+window.MTBlockEditor.registerBlockType(
+  window.MTBlockEditor.createBoilerplateBlock({
+    icon: "",
+    canRemoveBlock: 1,
+    typeId: "custom-gallery",
+    className: "",
+    addableBlockTypes: ["core-image"],
+    html: "",
+    shouldBeCompiled: 1,
+    previewHeader: `
+<style type="text/css">
+  .swiper-container {
+    display: flex;
+  }
+  img {
+    max-width: 100%;
+    height: auto;
+  }
+</style>
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/5.4.1/css/swiper.min.css" integrity="sha256-urV1luZ8JPIzBb+88vMT8qwbF8jBbHIrwQ8wTghtCOw=" crossorigin="anonymous" />
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/5.4.1/js/swiper.min.js" integrity="sha256-xKd8O6CvawXs3b8ZCrfuwN9A0ISCM8bUhJC86L6A8OY=" crossorigin="anonymous" defer></script>
+
+<script>
+  document.addEventListener("DOMContentLoaded", () => {
+    if (document.body.dataset.hasCompiledHtml) {
+    console.log(document.querySelector(".swiper-container"));
+      new Swiper(".swiper-container", {
+        loop: true,
+        pagination: {
+          el: ".swiper-pagination",
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      });
+
+      return;
+    }
+
+    const imgs = document.querySelectorAll("img");
+    const slides = [];
+    const sizes = [];
+    for (let i = 0; i < imgs.length; i++) {
+      const img = imgs[i];
+      img.style = {};
+      const elm = img.closest("figure") || img;
+      elm.style = {};
+      slides.push(\`<div class="swiper-slide">$\{elm.outerHTML}</div>\`);
+      sizes.push(img.width);
+    }
+
+    MTBlockEditorSetCompiledHtml(\`
+<div class="swiper-container">
+ <div class="swiper-wrapper">
+   $\{slides.join("")}
+ </div>
+ <div class="swiper-pagination"></div>
+
+ <div class="swiper-button-prev"></div>
+ <div class="swiper-button-next"></div>
+</div>
+\`);
+  });
+</script>
+    `,
+    label: "ギャラリー",
+    rootBlock: "",
+  })
+);
+
 MTBlockEditor.registerBlockType(
   MTBlockEditor.createBoilerplateBlock({
     typeId: "custom-excel",
     className: "excel",
-    label: "Table from Excel",
+    label: "エクセル",
     html: ``,
     canRemoveBlock: false,
     addableBlockTypes: [],
@@ -65,7 +137,7 @@ function excelToTable(file) {
       });
       html += "</tbody></table>";
 
-      MTBlockEditorSetCompiledHtml(html);
+      MTBlockEditorSetCompiledHtml(html, { addEditHistory: true });
     });
   };
 
@@ -92,6 +164,35 @@ document.addEventListener("DOMContentLoaded", async () => {
       excelToTable(files[i]);
     }
   });
+});
+</script>
+    `,
+  })
+);
+
+MTBlockEditor.registerBlockType(
+  MTBlockEditor.createBoilerplateBlock({
+    typeId: "custom-syntax_highlighting",
+    className: "syntax_highlighting",
+    label: "ソースコード",
+    html: ``,
+    canRemoveBlock: false,
+    addableBlockTypes: [],
+    shouldBeCompiled: true,
+    html:
+      '<!-- mt-beb t="sixapart-textarea" m="{&quot;blockElement&quot;:&quot;pre&quot;,&quot;formatter&quot;:&quot;none&quot;,&quot;text&quot;:&quot;&quot;,&quot;label&quot;:&quot;ソースコード&quot;,&quot;className&quot;:&quot;source&quot;}"--><pre class="source"></pre><!-- /mt-beb --><!-- mt-beb t="sixapart-select" m="{&quot;options&quot;:&quot;html\\njavascript&quot;,&quot;blockElement&quot;:&quot;p&quot;,&quot;text&quot;:&quot;html&quot;,&quot;label&quot;:&quot;言語&quot;,&quot;className&quot;:&quot;language&quot;}"--><p class="language">html</p><!-- /mt-beb -->',
+    previewHeader: `
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.18.0/styles/a11y-light.min.css" integrity="sha256-tJFLL8gFycTbUxiIYM6yLqldxB2YFdzB4viha8cVRsE=" crossorigin="anonymous" />
+<script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.18.1/highlight.min.js" integrity="sha256-eOgo0OtLL4cdq7RdwRUiGKLX9XsIJ7nGhWEKbohmVAQ=" crossorigin="anonymous" defer></script>
+<script>
+document.addEventListener("DOMContentLoaded", () => {
+  if (!document.body.dataset.hasCompiledHtml) {
+    const code = document.querySelector(".source").innerHTML;
+    const lang = document.querySelector(".language").textContent;
+    MTBlockEditorSetCompiledHtml('<pre><code class="' + lang + '">' + code + "</code></pre>");
+  } else {
+    hljs.initHighlightingOnLoad();
+  }
 });
 </script>
     `,
@@ -127,6 +228,87 @@ window.MTBlockEditor.registerBlockType(
     previewHeader:
       '<script src="https://cdnjs.cloudflare.com/ajax/libs/marked/1.1.0/marked.min.js" integrity="sha256-POA+Q3FC8tyo/jZhQrw40H5mKcWAABdxNeUQ/uhIm4U=" crossorigin="anonymous"></script>\r\n<script>\r\ndocument.addEventListener("DOMContentLoaded", () => {\r\n  if (document.body.dataset.hasCompiledHtml) {\r\n    return;\r\n  }\r\n\r\n  const source = document.querySelector(".source").innerHTML;\r\n  MTBlockEditorSetCompiledHtml(marked(source));\r\n});\r\n</script>',
     label: "Markdown",
+    rootBlock: "",
+  })
+);
+
+window.MTBlockEditor.registerBlockType(
+  window.MTBlockEditor.createBoilerplateBlock({
+    icon: "",
+    canRemoveBlock: "",
+    addableBlockTypes: [],
+    typeId: "custom-1header",
+    className: "",
+    html:
+      '<!-- mt-beb t="sixapart-input" m=\'{"blockElement":"h1","text":"","label":"大見出し","helpText":"H1で出力されます"}\' --><h1></h1><!-- /mt-beb -->',
+    shouldBeCompiled: "",
+    previewHeader: "",
+    label: "大見出し",
+    rootBlock: "",
+  })
+);
+
+window.MTBlockEditor.registerBlockType(
+  window.MTBlockEditor.createBoilerplateBlock({
+    icon: "",
+    canRemoveBlock: "",
+    addableBlockTypes: [],
+    typeId: "custom-2header",
+    className: "",
+    html:
+      '<!-- mt-beb t="sixapart-input" m=\'{"blockElement":"h2","text":"","label":"中見出し","helpText":"H2で出力されます"}\' --><h2></h2><!-- /mt-beb -->',
+    shouldBeCompiled: "",
+    previewHeader: "",
+    label: "中見出し",
+    rootBlock: "",
+  })
+);
+
+window.MTBlockEditor.registerBlockType(
+  window.MTBlockEditor.createBoilerplateBlock({
+    icon: "",
+    canRemoveBlock: "",
+    addableBlockTypes: [],
+    typeId: "custom-3header",
+    className: "",
+    html:
+      '<!-- mt-beb t="sixapart-input" m=\'{"blockElement":"h3","text":"","label":"小見出し","helpText":"H3で出力されます"}\' --><h3></h3><!-- /mt-beb -->',
+    shouldBeCompiled: "",
+    previewHeader: "",
+    label: "小見出し",
+    rootBlock: "",
+  })
+);
+
+window.MTBlockEditor.registerBlockType(
+  window.MTBlockEditor.createBoilerplateBlock({
+    icon: "",
+    canRemoveBlock: "",
+    addableBlockTypes: [],
+    typeId: "custom-paragraph",
+    className: "",
+    html:
+      '<!-- mt-beb t="sixapart-textarea" m=\'{"blockElement":"p","formatter":"nl2br","text":"","label":"段落","helpText":"Pで出力されます"}\' --><p></p><!-- /mt-beb -->',
+    shouldBeCompiled: "",
+    previewHeader: "",
+    label: "段落",
+    rootBlock: "",
+  })
+);
+
+window.MTBlockEditor.registerBlockType(
+  window.MTBlockEditor.createBoilerplateBlock({
+    icon: "",
+    canRemoveBlock: "",
+    addableBlockTypes: [],
+    typeId: "custom-image_text",
+    className: "",
+    html:
+      '<!-- mt-beb t="core-columns" m=\'{"className":"row"}\' --><div class="mt-be-columns row" style="display: flex"><!-- mt-beb t="core-column" m=\'{"className":"col-sm-6"}\' --><div class=\'mt-be-column col-sm-6\'><!-- mt-beb t="core-image" --><img/><!-- /mt-beb --></div><!-- /mt-beb --><!-- mt-beb t="core-column" m=\'{"className":"col-sm-6"}\' --><div class=\'mt-be-column col-sm-6\'><!-- mt-beb t="sixapart-textarea" m=\'{"blockElement":"p","formatter":"nl2br","text":""}\' --><p></p><!-- /mt-beb --></div><!-- /mt-beb --></div><!-- /mt-beb -->',
+    shouldBeCompiled: "",
+    previewHeader:
+      '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha256-aAr2Zpq8MZ+YA/D6JtRD3xtrwpEz2IqOS+pWD/7XKIw=" crossorigin="anonymous" />',
+    label: "画像とテキスト",
     rootBlock: "",
   })
 );
