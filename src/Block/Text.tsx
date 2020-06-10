6a577c1edef0d135701d0b1ebaea46889db4b958
Taku Amano <taku@taaas.jp>
2020-06-10 16:21:39 +0900

    The following options should be set by the user and are not specified here.
    * relative_urls
    * remove_script_host

--- src/Block/Text.tsx
+++ src/Block/Text.tsx
@@ -58,13 +58,10 @@ const Editor: React.FC<EditorProps> = ({
         "bullist numlist outdent indent | blockquote link unlink",
       ],
 
-      /* eslint-disable @typescript-eslint/camelcase */
+      // eslint-disable-next-line @typescript-eslint/camelcase
       fixed_toolbar_container: `#${block.tinymceId()}toolbar`,
       skin: "lightgray",
       inline: true,
-      relative_urls: false,
-      remove_script_host: true,
-      /* eslint-enable @typescript-eslint/camelcase */
 
       // eslint-disable-next-line @typescript-eslint/camelcase
       init_instance_callback: (ed: TinyMCE) => {
