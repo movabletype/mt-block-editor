876ad7479e116f6c54eb0a2f07e9673d575f7601
Taku Amano <taku@taaas.jp>
2020-10-15 17:24:44 +0900

    Update docs.

--- docs/register-block-with-custom-blocks.js
+++ docs/register-block-with-custom-blocks.js
@@ -3,7 +3,8 @@ MTBlockEditor.registerBlockType(
     typeId: "custom-oembed",
     className: "oembed",
     label: "oembed",
-    html: '<!-- mt-beb t="sixapart-input" m="{&quot;blockElement&quot;:&quot;pre&quot;,&quot;text&quot;:&quot;URLを入力してください&quot;,&quot;label&quot;:&quot;コンテンツのURL&quot;,&quot;helpText&quot;:&quot;https://noembed.comを使って、Flickrにも対応したバージョンです。&quot;,&quot;className&quot;:&quot;url&quot;}"--><pre class="url">URLを入力してください</pre><!-- /mt-beb --><!-- mt-beb t="sixapart-select" m="{&quot;options&quot;:&quot;640px\\n800px\\n指定しない&quot;,&quot;blockElement&quot;:&quot;p&quot;,&quot;text&quot;:&quot;640px&quot;,&quot;label&quot;:&quot;最大サイズ&quot;,&quot;className&quot;:&quot;size&quot;}"--><p class="size">640px</p><!-- /mt-beb -->',
+    html:
+      '<!-- mt-beb t="sixapart-input" m="{&quot;blockElement&quot;:&quot;pre&quot;,&quot;text&quot;:&quot;URLを入力してください&quot;,&quot;label&quot;:&quot;コンテンツのURL&quot;,&quot;helpText&quot;:&quot;https://noembed.comを使って、Flickrにも対応したバージョンです。&quot;,&quot;className&quot;:&quot;url&quot;}"--><pre class="url">URLを入力してください</pre><!-- /mt-beb --><!-- mt-beb t="sixapart-select" m="{&quot;options&quot;:&quot;640px\\n800px\\n指定しない&quot;,&quot;blockElement&quot;:&quot;p&quot;,&quot;text&quot;:&quot;640px&quot;,&quot;label&quot;:&quot;最大サイズ&quot;,&quot;className&quot;:&quot;size&quot;}"--><p class="size">640px</p><!-- /mt-beb -->',
     canRemoveBlock: false,
     addableBlockTypes: [],
     shouldBeCompiled: true,
@@ -173,5 +174,37 @@ document.addEventListener("DOMContentLoaded", async () => {
 });
 </script>
     `,
+  })
+);
+
+MTBlockEditor.registerBlockType(
+  MTBlockEditor.createBoilerplateBlock({
+    icon: "",
+    canRemoveBlock: 1,
+    typeId: "custom-contents",
+    addableBlockTypes: ["core-text", "core-html", "core-columns"],
+    className: "contents",
+    html: "",
+    shouldBeCompiled: "",
+    previewHeader: "",
+    label: "コンテンツ",
+    rootBlock: "",
+  })
+);
+
+MTBlockEditor.registerBlockType(
+  MTBlockEditor.createBoilerplateBlock({
+    icon: "",
+    canRemoveBlock: "",
+    typeId: "custom-bgcolor_contents",
+    addableBlockTypes: [],
+    className: "",
+    html:
+      '<!-- mt-beb t="sixapart-select" m=\'{"options":"白（#ffffff）\\n赤（#ff0000）\\n緑（#00ff00）\\n青（#0000ff）","blockElement":"p","text":"白（#ffffff）","label":"背景色","className":"color"}\' --><p class="color">白（#ffffff）</p><!-- /mt-beb --><!-- mt-beb t="custom-contents" --><!-- /mt-beb -->',
+    shouldBeCompiled: 1,
+    previewHeader:
+      '<script>\r\ndocument.addEventListener("DOMContentLoaded", async () => {\r\n  if (document.body.dataset.hasCompiledHtml) {\r\n    // 処理済み\r\n    return;\r\n  }\r\n\r\n  const colorElm = document.querySelector(".color");\r\n  colorElm.remove();\r\n\r\n  const color = colorElm.textContent.replace(/.*(#[0-9a-fA-F]+).*/, function(all, color) { return color});\r\n  const text = document.body.innerHTML;  \r\n\r\n  MTBlockEditorSetCompiledHtml(`\r\n    <div class="big-area" style="background-image: none; background-color: ${color};">\r\n      <div class="inner-wrap">\r\n          ${text}\r\n      </div>\r\n    </div>\r\n  `);\r\n});\r\n</script>',
+    label: "背景色 + コンテンツ",
+    rootBlock: "",
   })
 );
