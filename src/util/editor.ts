d6b0c479ec2dbfe14354cd718dbd9117ba20bdf8
Taku Amano <taku@taaas.jp>
2021-06-02 11:19:27 +0900

    Preserve 4bytes chars.

--- src/util/editor.ts
+++ src/util/editor.ts
@@ -31,7 +31,7 @@ export async function parseContent(
   const doc = domparser.parseFromString(
     `<xml>${value.replace(
       // eslint-disable-next-line no-control-regex
-      /[^\x09\x0A\x0D\x20-\xFF\x85\xA0-\uD7FF\uE000-\uFDCF\uFDE0-\uFFFD]/gm,
+      /[^\x09\x0A\x0D\x20-\xFF\x85\xA0-\uD7FF\uE000-\uFDCF\uFDE0-\uFFFD\uD800-\uDBFF\uDC00-\uDFFF]/gm,
       ""
     )}</xml>`,
     "application/xml"
