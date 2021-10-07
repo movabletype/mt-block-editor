d6225927e5662ce8f6a7350446c04fb329f47c04
Taku Amano <taku@taaas.jp>
2021-10-07 16:23:54 +0900

    Implement config modal demo.

--- src/Block/Image.tsx
+++ src/Block/Image.tsx
@@ -1,50 +1,38 @@
 import { t } from "../i18n";
-import React from "react";
+import React, { useState, useRef } from "react";
 import Block, { NewFromHtmlOptions, EditorOptions } from "../Block";
 import { nl2br } from "../util";
 import { blockProperty } from "../decorator";
 import icon from "../img/icon/image.svg";
+import iconLink from "../img/icon/link.svg";
 import BlockSetupCommon from "../Component/BlockSetupCommon";
 import BlockLabel from "../Component/BlockLabel";
+import BlockToolbar from "../Component/BlockToolbar";
+import BlockToolbarButton from "../Component/BlockToolbarButton";
+import {
+  Dialog,
+  DialogHeader,
+  DialogBody,
+  DialogFooter,
+} from "../Component/Dialog";
 
 interface EditorProps {
   block: Image;
 }
 
-const Editor: React.FC<EditorProps> = blockProperty(
-  ({ block }: EditorProps) => (
-    <div>
-      <BlockSetupCommon block={block} />
-      <BlockLabel block={block}>
-        <label className="mt-be-label-name">
-          <div>{t("Image URL")}</div>
-          <input type="url" name="url" data-mt-block-editor-focus-default />
-        </label>
-        <label className="mt-be-label-name">
-          <div>{t("Class Name")}</div>
-          <input name="className" />
-        </label>
-        <label className="mt-be-label-name">
-          <div>{t("Alternative Text")}</div>
-          <input name="alt" />
-        </label>
-        <label className="mt-be-label-name">
-          <div>{t("Caption")}</div>
-          <textarea
-            name="caption"
-            data-min-rows="1"
-            style={{ width: "100%" }}
-          ></textarea>
-        </label>
-      </BlockLabel>
-    </div>
-  )
-);
-
 const Html: React.FC<EditorProps> = ({ block }: EditorProps) => {
-  const img = (
-    <img src={block.url} className={block.className} alt={block.alt} />
-  );
+  let img = <img src={block.url} className={block.className} alt={block.alt} />;
+  if (block.linkUrl) {
+    img = (
+      <a
+        href={block.linkUrl}
+        target={block.linkTarget || undefined}
+        title={block.linkTitle || undefined}
+      >
+        {img}
+      </a>
+    );
+  }
 
   return block.caption ? (
     <figure>
@@ -55,6 +43,115 @@ const Html: React.FC<EditorProps> = ({ block }: EditorProps) => {
     <p>{img}</p>
   );
 };
+
+const Editor: React.FC<EditorProps> = blockProperty(
+  ({ block }: EditorProps) => {
+    const [isLinkDialogOpen, setLinkDialogOpen] = useState(false);
+    const formRef = useRef<HTMLFormElement>(null);
+
+    return (
+      <>
+        <BlockToolbar>
+          <BlockToolbarButton
+            icon={iconLink}
+            label={t("Insert Link")}
+            onClick={() => setLinkDialogOpen(true)}
+          />
+        </BlockToolbar>
+        <Dialog
+          open={isLinkDialogOpen}
+          onClose={() => setLinkDialogOpen(false)}
+        >
+          <DialogHeader>
+            <h4 className="mt-be-dialog-title">{t("Insert Link")}</h4>
+          </DialogHeader>
+          <form ref={formRef}>
+            <DialogBody>
+              <label className="mt-be-label-name">
+                <div className="mt-be-label-block">{t("Link URL")}</div>
+                <input
+                  type="url"
+                  className="mt-be-input"
+                  name="linkUrl"
+                  defaultValue={block.linkUrl}
+                  data-mt-block-editor-focus-default
+                />
+              </label>
+              <label className="mt-be-label-name">
+                <div className="mt-be-label-block">{t("Title")}</div>
+                <input
+                  className="mt-be-input"
+                  name="linkTitle"
+                  defaultValue={block.linkTitle}
+                />
+              </label>
+              <label className="mt-be-label-name">
+                <div className="mt-be-label-block">{t("Target Attribute")}</div>
+                <select
+                  name="linkTarget"
+                  className="mt-be-input"
+                  defaultValue={block.linkTarget}
+                >
+                  <option value="_self">{t("None")}</option>
+                  <option value="_blank">{t("New window")}</option>
+                </select>
+              </label>
+            </DialogBody>
+
+            <DialogFooter>
+              <button
+                type="button"
+                className="mt-be-btn-default"
+                onClick={() => {
+                  setLinkDialogOpen(false);
+                }}
+              >
+                {t("Close")}
+              </button>
+              <button
+                type="button"
+                className="mt-be-btn-primary"
+                onClick={() => {
+                  for (const e of (formRef.current?.elements ||
+                    []) as HTMLInputElement[]) {
+                    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                    (block as any)[e.name] = e.value;
+                  }
+                  setLinkDialogOpen(false);
+                }}
+              >
+                {t("Save")}
+              </button>
+            </DialogFooter>
+          </form>
+        </Dialog>
+        <BlockSetupCommon block={block} />
+        <BlockLabel block={block}>
+          <label className="mt-be-label-name">
+            <div>{t("Image URL")}</div>
+            <input type="url" name="url" data-mt-block-editor-focus-default />
+          </label>
+          <label className="mt-be-label-name">
+            <div>{t("Class Name")}</div>
+            <input name="className" />
+          </label>
+          <label className="mt-be-label-name">
+            <div>{t("Alternative Text")}</div>
+            <input name="alt" />
+          </label>
+          <label className="mt-be-label-name">
+            <div>{t("Caption")}</div>
+            <textarea
+              name="caption"
+              data-min-rows="1"
+              style={{ width: "100%" }}
+            ></textarea>
+          </label>
+        </BlockLabel>
+      </>
+    );
+  }
+);
 
 class Image extends Block {
   public static typeId = "core-image";
@@ -67,6 +164,9 @@ class Image extends Block {
   public url = "";
   public alt = "";
   public caption = "";
+  public linkUrl = "";
+  public linkTarget = "_self";
+  public linkTitle = "";
 
   public constructor(init?: Partial<Image>) {
     super();
