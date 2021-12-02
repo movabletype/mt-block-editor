cf4614c6be40f6d7bab48b7d1f5ce4894eab03bf
Taku Amano <taku@taaas.jp>
2021-12-02 13:17:30 +0900

    Add tests.

---
+++ cypress/blocks/src/i18n.ts
@@ -0,0 +1,14 @@
+import i18n from "../../../sub-projects/mt-block-editor-block/i18n";
+import { locales } from "../i18next-parser.config";
+
+i18n.on("initialized", () => {
+  locales.forEach((lang) => {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const l = require(`./locales/${lang}/translation.json`);
+    i18n.addResources(lang, "translation", l);
+  });
+});
+
+export function t(args: string | string[]): string {
+  return i18n.t(args);
+}
