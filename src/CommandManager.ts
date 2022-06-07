1b584329809062f0cd2d10c23ee7f3894990eae7
Taku Amano <taku@taaas.jp>
2022-06-07 13:05:53 +0900

    Update CommandManager.ts.

--- src/CommandManager.ts
+++ src/CommandManager.ts
@@ -1,13 +1,33 @@
 import EventEmitter from "eventemitter3";
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 
-import Editor from "./Editor";
+import type Editor from "./Editor";
 import type Block from "./Block";
-import { useEditorContext } from "./Context";
+import { useEditorContext, EditorContextProps } from "./Context";
+import { findDescendantBlock, toKeyboardShortcutKey } from "./util";
+import { DialogProps } from "./Component/Dialog";
 
-interface Command {
+export interface Command {
   command: string;
-  callback: (event: Event) => void;
+  callback?: (event: BlockEditorCommandEvent) => void;
+  label?: string | (() => string);
+  icon?: string;
+  shortcut?: string;
+  dialog?: React.FC<DialogProps>;
+}
+
+interface BlockEditorCommandEventDetail {
+  blocks: Block[];
+  editorContext: EditorContextProps;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  extra?: any;
+}
+export class BlockEditorCommandEvent extends CustomEvent<
+  BlockEditorCommandEventDetail
+> {
+  constructor(detail: BlockEditorCommandEventDetail) {
+    super("mt-be-command", { detail });
+  }
 }
 
 interface UseCommandsParams {
@@ -16,17 +36,26 @@ interface UseCommandsParams {
 }
 
 export default class CommandManager {
+  public static allCommands: Command[] = [];
   public editor: Editor;
   private eventEmitters: Record<string, EventEmitter> = {};
+
+  public static registerCommand(command: Command): void {
+    CommandManager.allCommands.push(command);
+  }
 
   public constructor(init: Pick<CommandManager, "editor">) {
     this.editor = init.editor;
   }
 
+  public commands(): Command[] {
+    return CommandManager.allCommands;
+  }
+
   public on(
     blockId: string,
     command: string,
-    callback: (event: Event) => void
+    callback: (event: BlockEditorCommandEvent) => void
   ): void {
     this.eventEmitters[blockId] ||= new EventEmitter();
     this.eventEmitters[blockId].on(command, callback);
@@ -45,18 +74,69 @@ export default class CommandManager {
   public dispatchKeydownEvent({
     event,
     blockId,
+    editorContext,
   }: {
     event: KeyboardEvent;
     blockId: string;
+    editorContext: EditorContextProps;
   }): void {
-    if (
-      event.key === "k" &&
-      (event.ctrlKey || event.metaKey) &&
-      !event.shiftKey
-    ) {
+    const key = toKeyboardShortcutKey(event);
+
+    if (key === "cmd+k") {
       event.preventDefault();
       this.emit(blockId, "core-insertLink");
+      return;
     }
+    if (key === "cmd+c") {
+      const s = window.getSelection();
+      if (s && !s.isCollapsed) {
+        return;
+      }
+    }
+
+    for (const command of this.editor.keyboardShortcuts()) {
+      if (command.shortcut === key) {
+        event.preventDefault();
+        const blocks = blockId
+          ?.split(",")
+          .map((id) => findDescendantBlock(this.editor, id))
+          .filter((b): b is Block => !!b);
+        command.callback &&
+          command.callback(
+            new BlockEditorCommandEvent({
+              blocks,
+              editorContext,
+            })
+          );
+        return;
+      }
+    }
+  }
+
+  public execute({
+    command,
+    blockId,
+    editorContext,
+  }: {
+    command: string;
+    blockId: string;
+    editorContext: EditorContextProps;
+  }): void {
+    this.commands().forEach((c) => {
+      if (c.command === command) {
+        const blocks = blockId
+          ?.split(",")
+          .map((id) => findDescendantBlock(this.editor, id))
+          .filter((b): b is Block => !!b);
+        c.callback &&
+          c.callback(
+            new BlockEditorCommandEvent({
+              blocks,
+              editorContext,
+            })
+          );
+      }
+    });
   }
 }
 
@@ -70,7 +150,9 @@ export function useCommands(
 
   useEffect(() => {
     for (const command of commands) {
-      CommandManager.on(block.id, command.command, command.callback);
+      if (command.callback) {
+        CommandManager.on(block.id, command.command, command.callback);
+      }
     }
 
     return () => {
