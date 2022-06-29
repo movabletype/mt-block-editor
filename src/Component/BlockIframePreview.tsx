b6608c41fa33d8c5f0434ebb1789e684f540a4ea
Taku Amano <taku@taaas.jp>
2022-06-29 15:32:35 +0900

    Fix typo.

--- src/Component/BlockIframePreview.tsx
+++ src/Component/BlockIframePreview.tsx
@@ -367,7 +367,7 @@ const BlockIframePreview: React.FC<EditorProps> = ({
             break;
           }
 
-          setSize({ size, ...ev.data.arguments });
+          setSize({ ...size, ...ev.data.arguments });
           break;
         case "MTBlockEditorSetSize":
           (Object.keys(size) as Array<keyof Size>).forEach((k) => {
