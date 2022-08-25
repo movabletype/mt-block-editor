dfc9fd57f08985db556a7057e0a74319d00704f7
Taku Amano <taku@taaas.jp>
2022-08-25 20:30:41 +0900

    Disable some functions again.

--- src/Component/BlockIframePreview.tsx
+++ src/Component/BlockIframePreview.tsx
@@ -347,6 +347,9 @@ const BlockIframePreview: React.FC<EditorProps> = ({
         <head>
           <meta charset="utf-8">
           <script>
+            ["alert", "confirm", "prompt"].forEach(function(name) {
+              window[name] = function() { console.log(name + " is disabled in a preview iframe") };
+            });
             setTimeout(${InitSizeFunc.toString()}, 50);
             setInterval(${postMessageFunc.toString()}, 1000);
             var MTBlockEditorSetCompiledHtml = ${setCompiledHtmlFunc.toString()};
