dad5ea0597c3faeaa85ec966a1d8c967eb928aa1
Taku Amano <taku@taaas.jp>
2026-01-13 09:04:51 +0900

    Use automatic JSX runtime for React 19 builds
    Configure @babel/preset-react to use the automatic runtime, which
    transforms JSX to _jsx/_jsxs calls from react/jsx-runtime instead of
    React.createElement.

--- babel.config.js
+++ babel.config.js
@@ -38,7 +38,7 @@ module.exports = function(api) {
           }
         },
       ],
-      "@babel/preset-react",
+      ["@babel/preset-react", { runtime: "automatic" }],
       "@babel/preset-typescript",
     ].filter(Boolean),
     plugins: [
