0dfa592cb4d6a8c792127cfe4c13ddf5b90d428a
Taku Amano <taku@taaas.jp>
2026-03-17 07:55:50 +0900

    Update mt-block-editor-block submodule

--- sub-projects/mt-block-editor-block/src/jsx-dev-runtime.ts
+++ sub-projects/mt-block-editor-block/src/jsx-dev-runtime.ts
@@ -1,5 +1,22 @@
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const jsxRuntime = window.MTBlockEditor.jsxRuntime as any;
+import type * as React from "react";
 
-export const jsxDEV = jsxRuntime.jsxDEV || jsxRuntime.jsx;
-export const Fragment = jsxRuntime.Fragment;
+const jsxRuntime = window.MTBlockEditor.jsxRuntime;
+
+export interface JSXSource {
+  fileName?: string | undefined;
+  lineNumber?: number | undefined;
+  columnNumber?: number | undefined;
+}
+
+export const jsxDEV: (
+  type: React.ElementType,
+  props: unknown,
+  key: React.Key | undefined,
+  isStatic: boolean,
+  source?: JSXSource,
+  self?: unknown
+) => React.ReactElement = jsxRuntime.jsxDEV || jsxRuntime.jsx;
+
+export const Fragment: typeof React.Fragment = jsxRuntime.Fragment;
+
+export type { JSX } from "react";
--- sub-projects/mt-block-editor-block/src/jsx-runtime.ts
+++ sub-projects/mt-block-editor-block/src/jsx-runtime.ts
@@ -1,5 +1,19 @@
+import type * as React from "react";
+
 const jsxRuntime = window.MTBlockEditor.jsxRuntime;
 
-export const jsx = jsxRuntime.jsx;
-export const jsxs = jsxRuntime.jsxs;
-export const Fragment = jsxRuntime.Fragment;
+export const jsx: (
+  type: React.ElementType,
+  props: unknown,
+  key?: React.Key
+) => React.ReactElement = jsxRuntime.jsx;
+
+export const jsxs: (
+  type: React.ElementType,
+  props: unknown,
+  key?: React.Key
+) => React.ReactElement = jsxRuntime.jsxs;
+
+export const Fragment: typeof React.Fragment = jsxRuntime.Fragment;
+
+export type { JSX } from "react";
